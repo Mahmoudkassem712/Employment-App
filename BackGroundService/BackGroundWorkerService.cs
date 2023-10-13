d159f06056e5eab87dd2336599a63f93ffeabd8d
mahmoud kassem <32333035+Mahmoudkassem712@users.noreply.github.com>
2023-10-13 16:00:18 +0300

    Add files via upload

---
+++ BackGroundService/BackGroundWorkerService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackGroundService
+{
+    public class BackGroundWorkerService : BackgroundService
+    {
+        private readonly ILogger<BackGroundWorkerService> _logger;
+
+        public BackGroundWorkerService(ILogger<BackGroundWorkerService> logger)
+        {
+            _logger = logger;
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Start Task of Archiving Vacancy");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Service Running at time : {DateTimeOffset.Now} ");
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
